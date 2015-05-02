712cb88fc473c01eb90362bc40340761f3224407
ikanyu <wongwy93@gmail.com>
2015-05-03 01:00:04 +0800

    Email freeze
    freeze email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :products
+
+  validate :freeze_email, :on => :update
+
+  attr_readonly :email
+
+  validates_format_of :email, :with => (/\A([^@\s]+)@nottingham.edu.my\z/)
+  #validates_format_of :email, :with => (/A^([^@\s]+)@(nottingham.edu.my)z/)
   #has_many :offered_prices, through: :products
 
   def mailboxer_email(object)
  #return the model's email here
-end
+  end
+
+  protected
+
+  def freeze_email
+  		errors.add(:email, "cannot be changed") if self.email_changed?
+  end	
 end
